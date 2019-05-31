4f9e714bb428b7f850c39bd3a32406af02f52bb5
TychoDesktop <TychoZaal@gmail.com>
2019-05-31 15:51:48 +0200

    Object pooling works

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -4,21 +4,19 @@ using UnityEngine;
 
 public class Bullet : MonoBehaviour
 {
-
 	public string killTag = "Player";
 	public int damage = 1;
 
 	public GameObject effect;
 
-	private void OnCollisionEnter2D(Collision2D collision)
+    private void OnCollisionEnter2D(Collision2D collision)
 	{
 		if(collision.collider.CompareTag(killTag))
 		{
 			collision.collider.GetComponent<Player>().TakeDamage(damage);
 		}
 
-		Instantiate(effect, transform.position, Quaternion.identity, transform.parent);
+        ObjectPooler.Instance.SpawnFromPool("Explosion", transform.position, Quaternion.identity);
 		Destroy(gameObject);
 	}
-
 }
---
+++ Assets/Scripts/ParticleInactive.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticleInactive : MonoBehaviour
+{
+    public bool needsToReset = false;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (needsToReset)
+        {
+            StartCoroutine(Reset());
+        }
+    }
+
+    IEnumerator Reset()
+    {
+        yield return new WaitForSeconds(2);
+        needsToReset = false;
+        gameObject.SetActive(false);
+    }
+}
