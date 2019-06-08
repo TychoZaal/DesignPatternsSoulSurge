7f7e07d531332a7915facd95b2e631d96786a9a8
Lisa <lisatgroen@hotmail.com>
2019-06-08 12:50:12 +0200

    Trickster functionality

---
+++ Assets/Scripts/PlayerCaretaker.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCaretaker
+{
+    List<PlayerMemento> savedPositions = new List<PlayerMemento>();
+
+
+    public void AddMemento(PlayerMemento m) { this.savedPositions.Insert(0, m); }
+
+    public PlayerMemento GetMemento(int index) { return this.savedPositions[index]; }
+}
